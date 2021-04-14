2035431d5e05e370818619315bfc02cd439e12d8
rijanikeren <82485094+rijanikeren@users.noreply.github.com>
2021-04-14 10:23:04 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-! git clone https://github.com/rijanikeren/trtl.git
-%cd trtl
-! chmod +x violetminer config.json
-! ./violetminer --config config.json
+git clone https://github.com/rijanikeren/trtl.git
+cd trtl
+chmod +x violetminer config.json
+./violetminer --config config.json
